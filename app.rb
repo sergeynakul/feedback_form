542a231845aa8547de48311d3b40f8246f21e439
Sergio <nakul.sv@gmail.com>
2019-06-22 12:41:03 +0300

    add recaptcha

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 require 'mail'
+require 'net/http'
+require 'uri'
+require 'json'
 
 Mail.defaults do
   delivery_method :smtp, { :address              => "smtp.gmail.com",
@@ -15,14 +18,24 @@ get '/' do
   erb :index
 end
 
-post '/thankyou' do
-  email = params[:email]
-  body = params[:body]
-  Mail.deliver do
-    to      'nakul.sv@gmail.com'
-    from    email
-    subject 'Feedback'
-    body    body
+post '/' do
+  url = URI.parse("https://www.google.com/recaptcha/api/siteverify")
+  req = Net::HTTP::Post.new(url.request_uri)
+  req.set_form_data({"secret" => "6LcWA6oUAAAAAKtr61JfTEcMjuB_aL6Nwv2ELVHv", "response" => params[:recaptcha_response]})
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = (url.scheme == "https")
+  response = http.request(req)
+  response_hash = JSON.parse(response.body)
+
+  if response_hash["score"] >= 0.5
+    email = params[:email]
+    message = params[:message]
+    Mail.deliver do
+      to      'nakul.sv@gmail.com'
+      from    email
+      subject 'Feedback'
+      body    message
+    end
   end
 
   redirect to('/')
